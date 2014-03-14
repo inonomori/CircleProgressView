8220d877d66c47c8726dc9057fc7ad0e402a5f75
nonomori <nonomori@gmail.com>
2014-03-14 16:05:20 +0800

    Initial Commit

---
+++ CircleProgress/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CircleProgress
+//
+//  Created by zhefu wang on 3/14/14.
+//  Copyright (c) 2014 zhefu wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CircleProgress/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CircleProgress
+//
+//  Created by zhefu wang on 3/14/14.
+//  Copyright (c) 2014 zhefu wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
